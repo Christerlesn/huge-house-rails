408b8204cdb3f7d4d03c23ef1f068f216000247a
Christerle_Nounoute <christerlesn@gmail.com>
2021-05-30 15:23:18 -0700

    created sessions controller & homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
+
+  get '/' => 'sessions#welcome'
+  
   resources :vendors
   resources :reservations
   resources :events
   resources :clients
+  resources :sessions
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
