1613fced48a3aefb6e59d4f6635fe4ada55fe0f9
Christerle_Nounoute <christerlesn@gmail.com>
2021-05-30 14:47:38 -0700

    added assoications, and user validations

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,7 @@
 class Client < ApplicationRecord
+    has_many :reservations
+    has_many :events, through: :reservations  
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    has_secure_password
 end
