bb4d8ff53441035a7b5ab9c1c56337b1a7cf5709
Christerle_Nounoute <christerlesn@gmail.com>
2021-05-30 15:48:29 -0700

    Creating Sign Up

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,26 @@
 class ClientsController < ApplicationController
+
+    def new
+        @client = Client.new
+    end
+
+    def create
+        @client = Client.new(client_params)
+        if @client.save
+            redirect_to client_path(@client)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @client = Client.find_by_id(params[:id])
+         redirect_to '/' if !@client
+    end
+
+    private
+
+    def client_params
+        params.require(:client).permit(:username, :first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,5 +2,4 @@ class SessionsController < ApplicationController
 
     def welcome
     end
-    
 end
