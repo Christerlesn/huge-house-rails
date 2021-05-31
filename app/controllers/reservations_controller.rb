ad34c18e5079bed4ecca5026eb582b5e02d7e370
Christerle_Nounoute <christerlesn@gmail.com>
2021-05-31 09:09:12 -0700

    Created Selection of Existing Events in Reservation#new

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -8,13 +8,16 @@ class ReservationsController < ApplicationController
     def create
         @reservation = Reservation.new(reservation_params)
         @reservation.client_id = session[:client_id]
-        if @reservation.save!
+        if @reservation.save
             redirect_to reservation_path(@reservation)
         else
             render :new
         end
     end
 
+    def show
+        @reservation = Reservation.find_by_id(params[:id])
+    end
     private
     def reservation_params
         params.require(:reservation).permit(:start_time, :end_time, :client_id, :event_id, event_attributes:[:title, :description])
