94a309e49d462d6a923cb7349fd302b969df29f9
Christerle_Nounoute <christerlesn@gmail.com>
2021-05-30 18:26:06 -0700

    Trying to create Nested form in reservations#new

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -2,9 +2,22 @@ class ReservationsController < ApplicationController
 
     def new
         @reservation = Reservation.new
+        @reservation.build_event
     end
 
     def create
+        @reservation = Reservation.new(reservation_params)
+        @reservation.client_id = session[:client_id]
+        if @reservation.save
+            redirect_to reservation_path(@reservation)
+        else
+            flash[:error] = "Something is wrong with this Reservation. Please fix it before we continue."
+            render :new
+        end
+    end
 
+    private
+    def reservation_params
+        params.require(:reservation).permit(:start_time, :end_time, :user_id, :event_id)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,13 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   
+  resources :clients, only: [:new, :create, :show]
+
+  resources :reservations do
+    resources :events, only: [:new, :create]
+  end
   resources :vendors
-  resources :reservations
-  resources :events
-  resources :clients
-  resources :sessions
+  
 
   delete '/logout' => 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
