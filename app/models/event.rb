f21d2db5e7fbbd3f7c632006f87e7a4817b445a0
Christerle_Nounoute <christerlesn@gmail.com>
2021-06-05 14:22:31 -0700

    Created Error partial page, and made some validations

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,7 @@
 class Event < ApplicationRecord
     has_many :vendors
     has_many :reservations
-    has_many :clients, through: :reservations  
+    has_many :clients, through: :reservations
+    validates :title, presence: true, uniqueness: true
+    validates :description, presence: true
 end
