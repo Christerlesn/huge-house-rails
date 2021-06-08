02f6f9e1b91e99be631a2da4bce12c14e5ca0d91
Christerle_Nounoute <christerlesn@gmail.com>
2021-06-07 21:49:25 -0700

    Creating Omniauth connection using github

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,24 +8,22 @@ class SessionsController < ApplicationController
     end
 
     def create
-        if params[:provider] == 'github'
-            @client = Client.find_by_create_id(uid: auth['uid']) do |c|
-                c.email = auth['info']['email']
-                c.first_name = auth['info']['name']
-            end
+        @client = Client.find_by(username: params[:client][:username])
+        if @client && @client.authenticate(params[:client][:password])
             session[:client_id] = @client.id
+            flash[:message] = "Welcome #{@client.first_name}"
             redirect_to reservations_path
         else
-            @client = Client.find_by(username: params[:client][:username])
-            if @client && @client.authenticate(params[:client][:password])
-                session[:client_id] = @client.id
-                flash[:message] = "Welcome #{@client.first_name}"
-                redirect_to reservations_path
-            else
-                flash[:error] = "The Username or Password is Incorrect, or doesn't exist"
-                render :new
-            end
+            flash[:error] = "The Username or Password is Incorrect, or doesn't exist"
+            render :new
         end
+    end
+
+    def omniauth
+        @client = Client.create_by_github_omniauth(auth)
+
+        session[:client_id] = @client.id
+        redirect_to reservations_path
     end
 
     def destroy
--- app/models/client.rb
+++ app/models/client.rb
@@ -7,10 +7,9 @@ class Client < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     has_secure_password
 
-    def self.create_by_github_omniauth(auth)
-        self.find_or_create_by(username: auth[:info][:email]) do |u|
-          u.password = SecureRandom.hex
-    
-        end
+  def self.create_by_github_omniauth(auth)
+    self.find_or_create_by(username: auth[:info][:login]) do |u|
+      u.password = SecureRandom.hex
     end
+  end
 end
