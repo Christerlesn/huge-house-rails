b3b2395f8edf1c01544713d5b000a0017bd02a54
Christerle_Nounoute <christerlesn@gmail.com>
2021-06-08 22:41:19 -0700

    Created if statements to prevent a person who is logged in to be able to go back to the sign in or log in and cause a glitch. At least I think so.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,11 @@
 class ClientsController < ApplicationController
 
     def new
-        @client = Client.new
+        if current_user
+            redirect_to reservations_path
+        else
+            @client = Client.new
+        end
     end
 
     def create
