dae4ac05c0de9294d984b03765a997616e9172b4
Christerle_Nounoute <christerlesn@gmail.com>
2021-06-09 10:38:46 -0700

    Put restricitons on reservations/show, dried up sessions controller omniauth in the create action, and added more logic in the client model for the sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,24 +11,11 @@ class SessionsController < ApplicationController
 
     def create
         if auth_hash = request.env["omniauth.auth"]
-            # raise auth_hash.inspect
-            oauth_username = request.env['omniauth.auth']['info']['nickname']
-            if @client = Client.find_by(:username => oauth_username)
-                session[:client_id] = @client.id
+            @client = Client.find_or_create_by_omniauth(auth_hash)
+            session[:client_id] = @client.id
 
-                flash[:message] = "Welcome #{@client.username}"
-                redirect_to reservations_path
-            else
-                @client = Client.new(:username => oauth_username, :first_name => request.env['omniauth.auth']['info']['name'], :password => SecureRandom.hex, :email => request.env['omniauth.auth']['info']['urls']['GitHub'])
-                if @client.save
-                    session[:client_id] = @client.id
-
-                    flash[:message] = "Welcome #{@client.username}"
-                    redirect_to reservations_path
-                else
-                    raise @client.errors.full_messages.inspect
-                end
-            end
+            flash[:message] = "Welcome #{@client.username}"
+            redirect_to reservations_path
         else
             @client = Client.find_by(username: params[:client][:username])
             if @client && @client.authenticate(params[:client][:password])
--- app/models/client.rb
+++ app/models/client.rb
@@ -3,13 +3,14 @@ class Client < ApplicationRecord
     has_many :events, through: :reservations  
     validates :username, presence: true, uniqueness: true
     validates :first_name, presence: true
-    # validates :last_name, presence: true
     validates :email, presence: true, uniqueness: true
     has_secure_password
-
-  # def self.create_by_github_omniauth(auth)
-  #   self.find_or_create_by(username: auth[:info][:login]) do |u|
-  #     u.password = SecureRandom.hex
-  #   end
-  # end
+  
+  def self.find_or_create_by_omniauth(auth_hash)
+    self.where(:username => auth_hash['info']['nickname']).first_or_create do |client|
+      client.first_name = auth_hash['info']['name']
+      client.email = auth_hash['info']['urls']['GitHub']
+      client.password = SecureRandom.hex
+    end
+  end
 end
